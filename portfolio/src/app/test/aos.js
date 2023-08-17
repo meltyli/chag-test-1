de30b559d68b00e7653a91e34e76d0020bcf8380
meltyli <n9927379@qut.edu.au>
2023-08-17 11:23:05 +1000

    Auto-generated commit [ci skip]

--- portfolio/src/app/test/aos.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    AOS.init({
-        duration: 800, // Animation duration
-        easing: "ease-in-out", // Easing for animation
-    });
-});
